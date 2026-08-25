4dd3ea1119cb8fa271bf48629af659213e406d72
Ashok Kumar <kumarashok91042@gmail.com>
2026-08-25 10:31:38 +0530

    first commit

---
+++ Experiment-5/ReactJS5ci/src/App.jsx
@@ -0,0 +1,12 @@
+import Parent from './Parent';
+
+function App() {
+  return (
+    <div>
+      <h1>React Nesting Example</h1>
+      <Parent />
+    </div>
+  );
+}
+
+export default App;
---
+++ Experiment-5/ReactJS5cii/src/App.jsx
@@ -0,0 +1,10 @@
+import Classparent from './Classparent';
+
+function App() { return (
+<div>
+<h1>React Nesting Example</h1>
+<Classparent/>
+</div>
+);
+}
+export default App;
